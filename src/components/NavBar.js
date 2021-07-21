045d7fdd3b48adbe749398d15c87724a55d8bb60
Simon Boeger <simon@boegerweb.com>
2021-07-21 14:37:58 -0700

    first

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export function NavBar(props) {
+  return (
+    <div>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Whatup</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
